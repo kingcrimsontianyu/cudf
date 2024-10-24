2497fc032f8bcec3b951cf114c41ceeac4f6e56b
Tianyu Liu <kingcrimsontianyu@gmail.com>
2024-10-24 18:10:28 +0000

    Update

--- debug/run.sh
+++ debug/run.sh
@@ -8,7 +8,7 @@ color_green='\e[1;32m'
 export CUDF_BENCHMARK_DROP_CACHE=true
 
 # Only allow GDS IO. Disallow POSIX fallback.
-# export CUFILE_ALLOW_COMPAT_MODE=false
+export CUFILE_ALLOW_COMPAT_MODE=false
 
 export KVIKIO_NTHREADS=8
 export KVIKIO_COMPAT_MODE=ON
@@ -31,10 +31,10 @@ export CUFILE_LOGGING_LEVEL=TRACE
 # parquet_read_io_compression
 #-----------------------------------
 ${parquet_reader_bench_bin} -d 0 -b parquet_read_io_compression \
--a compression_type=NONE -a io_type=FILEPATH -a cardinality=1000 -a run_length=1 --min-samples 40
+-a compression_type=NONE -a io_type=FILEPATH -a cardinality=0 -a run_length=1 --min-samples 40
 
 #-----------------------------------
 # parquet_read_io_small_mixed
 #-----------------------------------
 # ${parquet_reader_bench_bin} -d 0 -b parquet_read_io_small_mixed \
-# -a io_type=FILEPATH -a num_string_cols=3 -a cardinality=1000 -a run_length=1 --min-samples 40
+# -a io_type=FILEPATH -a num_string_cols=3 -a cardinality=0 -a run_length=1 --min-samples 40

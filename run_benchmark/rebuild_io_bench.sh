3a4c225d39e374248a877c01b7b85e62bd0372a1
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-02-13 17:16:35 -0500

    Update run bench scripts

---
+++ run_benchmark/rebuild_io_bench.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+configure-cudf-cpp -DCMAKE_CUDA_ARCHITECTURES=native -DBUILD_BENCHMARKS=ON -DBUILD_TESTS=ON
+
+# cmake --build /home/coder/cudf/cpp/build/latest -j 10 -t PARQUET_READER_NVBENCH
+cmake --build /home/coder/cudf/cpp/build/latest -j 10 -t ORC_READER_NVBENCH
+# cmake --build /home/coder/cudf/cpp/build/latest -j 10 -t JSON_READER_NVBENCH
+# cmake --build /home/coder/cudf/cpp/build/latest -j 10 -t CSV_READER_NVBENCH
